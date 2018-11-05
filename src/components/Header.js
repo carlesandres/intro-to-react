5dfffbc7afa82bf045ad41855cce074631e08ea9
Carles Andres <carlos.andres.c@gmail.com>
2018-11-05 16:47:44 +0000

    Convert Header to a class component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Header = (props) => {
-  return ([
-    <h1>{props.title || 'Finantial Times'}</h1>,
-    <h2>{props.subtitle || ''}</h2>
-  ]);
-};
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return ([
+      <h1>{this.props.title || 'Finantial Times'}</h1>,
+      <h2>{this.props.subtitle || ''}</h2>
+    ]);
+  }
+}
 
 export default Header;
-
