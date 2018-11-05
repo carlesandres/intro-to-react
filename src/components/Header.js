76fff12057c4410b5bdf8a7b00b6cd1b24a7c98e
Carles Andres <carlos.andres.c@gmail.com>
2018-11-05 17:29:10 +0000

    Added "likes" counter to Header"

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,25 @@ import React from 'react';
 class Header extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      likes: 0
+    };
+  }
+
+  componentDidMount() {
+    // We are increasing the number of likes by 1 every second
+    setInterval( () => {
+      const newState = this.state.likes + 1;
+      this.setState({ likes: newState });
+    }, 1000);
   }
 
   render() {
     return ([
       <h1>{this.props.title || 'Finantial Times'}</h1>,
-      <h2>{this.props.subtitle || ''}</h2>
+      <h2>{this.props.subtitle || ''}</h2>,
+      <div>Likes: {this.state.likes}</div>
     ]);
   }
 }
